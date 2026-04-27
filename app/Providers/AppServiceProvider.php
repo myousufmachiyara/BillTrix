7dd5f697cc555d2e84294777462a65ff44568481
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2026-04-27 17:38:17 +0500

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,24 +1,16 @@
 <?php
-
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
+    public function register(): void {}
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        // Fix "Specified key was too long" on MySQL < 5.7.7 / MariaDB < 10.2.2
+        Schema::defaultStringLength(191);
     }
 }
